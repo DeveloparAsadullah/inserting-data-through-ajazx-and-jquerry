fffb9a6a01dc6e2bedcb4d3a7dba34a4302450e5
Asadullah <95171035+DeveloparAsadullah@users.noreply.github.com>
2021-11-28 18:08:53 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ajax_jquerry_26_nov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ajax_jquerry_26_nov.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        question_insertEntities db = new question_insertEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(tbl_insert i)
+        {
+            db.tbl_insert.Add(i);
+            db.SaveChanges();
+
+
+            return View();
+        }
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
